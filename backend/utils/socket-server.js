a61867e266f01198b70282896e159a82f530cf03
Umar <umarzzaib2@gmail.com>
2025-12-14 13:46:31 +0500

    Socket server configured. Working properly now!

--- backend/utils/socket-server.js
+++ backend/utils/socket-server.js
@@ -1,23 +1,25 @@
-let Server= require('socket.io').Server;
+let Server = require('socket.io').Server;
 
-let port=3000;
-let io_option={
+let port = 3000;
+let io_option = {
   cors: {
-    origin: ["https://localhost:5173"],
+    origin: "http://localhost:5173", // Make sure this is the exact URL of your frontend app
+    methods: ["GET", "POST"],
     allowedHeaders: ["my-custom-header"],
-    credentials: true
+    credentials: true // This enables cookies and other credentials to be sent
   }
-}
+};
 
-let io=new Server(io_option);
+let io = new Server(io_option);
 console.log(io);
 
-io.on('connection', (socket)=>{
-   console.log( '=> User connected in with id: ', socket.id );
+io.on('connection', (socket) => {
+   console.log('=> User connected with id: ', socket.id);
+
+   socket.on('disconnect', () => {
+     console.log('User disconnected with id: ', socket.id);
+   });
 });
 
-io.on('disconnect', (socket)=>{
-  console.log( 'User disconnected with id: ', socket.id );
-})
-
 io.listen(port);
+console.log('Socket server listening on port 3000');
