86e89860f651ee6a6627307810ae3baca8201638
Umar <umarzzaib2@gmail.com>
2025-12-15 16:51:52 +0500

    Loader added for AuthProvider

--- frontend/src/utils/AuthProvider.jsx
+++ frontend/src/utils/AuthProvider.jsx
@@ -1,11 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useLayoutEffect, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router';
+import Loader from './Loader.jsx';
 
 export default function AuthProvider({ children }) {
   const navigate = useNavigate();
   const baseUrl = 'http://localhost:8000';
   const [token, setToken] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   // Fetch access token on component mount
   useEffect(() => {
@@ -13,6 +15,7 @@ export default function AuthProvider({ children }) {
       .get(`${baseUrl}/user/get_access_token`, { withCredentials: true })
       .then((response) => {
         if (response.status === 202) { // HttpStatusCode.Accepted => 202
+          console.log(response.data.access_token);
           setToken(response.data.access_token);
         }
       })
@@ -22,21 +25,26 @@ export default function AuthProvider({ children }) {
         } else {
           alert(e.message);
         }
+      }).finally(()=>{
+        setLoading(false);
       });
+
   }, [navigate]);
 
   // Axios request interceptor for adding Authorization header
-  useEffect(() => {
+  useLayoutEffect(() => {
     const myInterceptor = axios.interceptors.request.use(
       (config) => {
         if (token) {
           config.headers.Authorization = `Bearer ${token}`;
+          console.log("token set");
         }
         return config;
       },
       (error) => {
         return Promise.reject(error);
       }
+
     );
 
     // Clean up the interceptor on unmount or token change
@@ -44,6 +52,10 @@ export default function AuthProvider({ children }) {
       axios.interceptors.request.eject(myInterceptor);
     };
   }, [token]); // Re-run this effect when token changes
+
+if (loading) {
+  return <Loader></Loader>
+}
 
   // Return wrapped children
   return <>{children}</>;
---
+++ frontend/src/utils/Loader.jsx
@@ -0,0 +1,10 @@
+// Loader.jsx
+import './loader.css';
+
+export default function Loader() {
+  return (
+    <div className="loader-overlay">
+      <div className="spinner"></div>
+    </div>
+  );
+}
